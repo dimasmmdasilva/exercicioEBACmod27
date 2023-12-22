e5a261b925376113c88b87b92a8b2247782b9f26
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2023-12-22 15:29:28 -0300

    refatoração exercício EBAC mod 27 aula crie componentes

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,43 @@
 <template>
     <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-        <h1>Minhas tarefas</h1>
-        <p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
-    </header>
-    <form @submit.prevent="cadastraTarefa">
-        <div class="row">
-        <div class="col">
-            <input v-model="estado.tarefaTemp" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-1">
-            <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-            <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-            <option value="todas">Todas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-            </select>
-        </div>
-        </div>
-    </form>
-    <ul class="list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()" :key="tarefa.título">
-        <input @change="evento => tarefa.finalizadas = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.título" type="checkbox">
-        <label :class="{ done: tarefa.finalizadas }" class="ms-3" :for="tarefa.título">
-            {{ tarefa.título }}
-        </label>
-        </li>
-    </ul>
+        <Cabeçalho :tarefas-pendentes="getTarefasPendentes.length" />
+        <Formulário @trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp"
+            @edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" @cadastra-tarefa="cadastraTarefa" />
+        <ListaDeTarefas :tarefas="getTarefasFiltradas()" />
     </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue';
+import Cabeçalho from './components/Cabeçalho.vue';
+import Formulário from './components/Formulário.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
 
 const estado = reactive
 ({
     filtro: 'todas',
     tarefaTemp: '',
     tarefas: [
-    { título: 'Estudar VueJS', finalizada: false },
-    { título: 'Caminhada na rua', finalizada: false },
-    { título: 'Treinamento físico', finalizada: false },
+        { título: 'Estudar VueJS', finalizada: false },
+        { título: 'Caminhada na rua', finalizada: false },
+        { título: 'Treinamento físico', finalizada: false },
     ],
 });
 
 const getTarefasPendentes = () =>
 {
-    return estado.tarefas.filter(tarefa => !tarefa.finalizada);};
+    return estado.tarefas.filter(tarefa => !tarefa.finalizada);
+};
 
 const getTarefasFinalizadas = () =>
 {
     return estado.tarefas.filter(tarefa => tarefa.finalizada);
 };
+
 const getTarefasFiltradas = () =>
 {
-const { filtro } = estado;
-    switch (filtro)
-    {
+    const { filtro } = estado;
+    switch (filtro) {
         case 'pendentes':
             return getTarefasPendentes();
         case 'finalizadas':
@@ -71,17 +50,10 @@ const { filtro } = estado;
 const cadastraTarefa = () =>
 {
     const tarefaNova = {
-    título: estado.tarefaTemp,
-    finalizada: false,
+        título: estado.tarefaTemp,
+        finalizada: false,
     };
     estado.tarefas.push(tarefaNova);
     estado.tarefaTemp = '';
 };
 </script>
-
-<style scoped>
-.done
-{
-    text-decoration: line-through;
-}
-</style>
---
+++ src/components/Formulário.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['tarefaTemp']);
+const emits = ['trocar-filtro', 'edita-tarefa-temp', 'cadastra-tarefa'];
+</script>
+
+<template>
+    <form @submit.prevent="$emit('cadastra-tarefa')">
+        <div class="row">
+            <div class="col">
+                <input v-model="props.tarefaTemp" @change="$emit('edita-tarefa-temp', $event)" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="$emit('trocar-filtro', $event)" class="form-control">
+                    <option value="todas">Todas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
